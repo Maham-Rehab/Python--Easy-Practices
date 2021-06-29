b2c2c36d0a434619302e61f3459a3f3ae8736314
Maham-Rehab <85940652+Maham-Rehab@users.noreply.github.com>
2021-06-29 16:47:48 +0500

    Add files via upload

---
+++ item_entry.py
@@ -0,0 +1,6 @@
+item=int()
+tol_item=int(input('enter your number of item'))
+for i in range(tol_item):
+    user_input=int(input('enter next'))
+    item+=user_input
+    print("item you added",user_input)
