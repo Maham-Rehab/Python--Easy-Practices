e051534b7a622dc98580873f7cbb321a93e9b2f2
Maham-Rehab <85940652+Maham-Rehab@users.noreply.github.com>
2021-07-26 16:48:23 +0500

    Add files via upload

---
+++ vowelrev.py
@@ -0,0 +1,14 @@
+a_string = input('enter string')
+count=0
+for vol in a_string:
+	if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+ 	count=count+1
+print(count)
+index=len(a_string)
+print(index)
+while index>0:
+	print(a_string[index-1])
+	print(index) 
+	index=index-1
+	print(index) 
+print(a_sring[::-1])                                                                                                                                                                                               print(i)
